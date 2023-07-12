51e0016ada18a29085a3ec48e0763b11ff1ae941
Roman Firsov <firsov_r@stdpr.ru>
2023-07-12 12:11:48 +0800

    в качестве ответа будет поток данных;

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -5,11 +5,13 @@ import io.grpc.ManagedChannelBuilder;
 import org.example.grpc.GreetingServiceGrpc;
 import org.example.grpc.GreetingServiceOuterClass;
 
+import java.util.Iterator;
+
 public class Client {
 
     public static void main(String[] args) {
-            ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
-                .usePlaintext().build();
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+            .usePlaintext().build();
 
         GreetingServiceGrpc.GreetingServiceBlockingStub stub =
                 GreetingServiceGrpc.newBlockingStub(channel);
@@ -17,9 +19,11 @@ public class Client {
         GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass.HelloRequest
                 .newBuilder().setName("Roman").build();
 
-        GreetingServiceOuterClass.HelloResponse response = stub.greeting(request);
+        Iterator<GreetingServiceOuterClass.HelloResponse> response = stub.greeting(request);
 
-        System.out.println(response);
+        while (response.hasNext()) {
+            System.out.println(response.next());
+        }
 
         channel.shutdownNow();
     }
